a6b705856ae585c815c65979222ddee57ce99682
Kimmi Gan <kimmi.gan@onfido.com>
2020-06-03 16:05:09 +0100

    create index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -10,12 +10,14 @@ async function run() {
     const octokit = new github.GitHub(token);
 
     const newIssue = await octokit.issues.create({
-        repo: github.context.repo.repo,
-        owner: github.context.repo.owner,
-        title: issueTitle,
-        body: jokeBody
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: jokeBody
     });
-  } catch(error) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
+
+run();
