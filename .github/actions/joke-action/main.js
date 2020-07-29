5eb29851996a804582e48957fffad1929ea3d99e
Kimmi Gan <kimmi.gan@onfido.com>
2020-07-29 21:19:24 +0100

    Add to string

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -3,7 +3,7 @@ const core = require("@actions/core");
 
 async function run() {
   const joke = await getJoke();
-  console.log(joke);
+  console.log("JOKE:", joke);
   core.setOutput("joke-output", joke);
 }
 
